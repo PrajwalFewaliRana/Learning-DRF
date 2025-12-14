2c60bc1df1a14696fcbb0eef769c21b3a651f57c
PrajwalFewaliRana <prajwalmagar68@gmail.com>
2025-12-14 22:48:01 +0545

    web endpoint,api endpoint ,models,,fetching static and dynamic api

---
+++ api/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse,JsonResponse
+from django.shortcuts import render
+from students.models import Student
+
+# Create your views here.
+
+def studentsView(request):
+    students = Student.objects.all()
+    students_list = list(students.values())
+    return JsonResponse(students_list,safe=False)
--- django_rest_framework_main/urls.py
+++ django_rest_framework_main/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    #web application endpoints
+    path('students/',include('students.urls')),
+    
+    #API Endpoints
+    path('api/v1/',include('api.urls'))
 ]
